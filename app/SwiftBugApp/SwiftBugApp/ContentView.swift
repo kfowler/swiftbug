3b71b9db4e6d40857a1720ba1f25c135004c3cf4
Kevin Fowler <kfowler@esterdome.com>
2020-03-26 20:34:32 -0700

    more bad

---
+++ app/SwiftBugApp/SwiftBugApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftBugApp
+//
+//  Created by Kevin Fowler on 3/26/20.
+//  Copyright © 2020 Imagine Financial, Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
